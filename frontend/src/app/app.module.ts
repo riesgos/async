a92009330fac08d218c5aab5fa7bf2688cfa938c
boec_mt <Mathias.Boeck@dlr.de>
2022-07-25 16:53:00 +0200

    add ukis frontend layout #2

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,17 +3,30 @@ import { BrowserModule } from "@angular/platform-browser";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { ClarityModule } from "@clr/angular";
 import { AppComponent } from "./app.component";
+import { HeaderComponent } from './components/header/header.component';
+import { GlobalAlertComponent } from './components/global-alert/global-alert.component';
+import { AlertService } from './components/global-alert/alert.service';
+import { GlobalProgressComponent } from './components/global-progress/global-progress.component';
+import { ProgressService } from './components/global-progress/progress.service';
+import { ExampleViewComponent } from './views/example-view/example-view.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    GlobalAlertComponent,
+    GlobalProgressComponent,
+    ExampleViewComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     ClarityModule,
   ],
-  providers: [],
+  providers: [
+    AlertService,
+    ProgressService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
