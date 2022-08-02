c01bab34709853a2392916f3c36f71427445b3bf
Arne Vogt <a.vogt@52north.org>
2022-08-02 15:55:26 +0200

    rewrite in/output mappers

--- asyncwrapper/src/main/kotlin/org/n/riesgos/asyncwrapper/process/wps/InputMapper.kt
+++ asyncwrapper/src/main/kotlin/org/n/riesgos/asyncwrapper/process/wps/InputMapper.kt
@@ -3,36 +3,32 @@ package org.n.riesgos.asyncwrapper.process.wps
 import org.n.riesgos.asyncwrapper.datamanagement.models.ComplexInput
 import org.n.riesgos.asyncwrapper.datamanagement.models.ComplexInputAsValue
 import org.n.riesgos.asyncwrapper.datamanagement.models.LiteralInput
+import org.n.riesgos.asyncwrapper.process.InlineParameter
 import org.n.riesgos.asyncwrapper.process.ProcessInput
+import org.n.riesgos.asyncwrapper.process.ReferenceParameter
 
-abstract class InputMapper (private val complexRefInputs: List<ComplexInput>, private val complexValInputs: List<ComplexInputAsValue>, private val literalInputs: List<LiteralInput>) {
+class InputMapper (var wpsProcessIdentifier : String) {
 
-    abstract fun mapInputs() : List<ProcessInput>
+    private val defaultTextMimeType = "text/xml"
 
-    protected fun getComplexRefInputByName(name : String) : ComplexInput?{
-        for (input in complexRefInputs) {
-            if (input.wpsIdentifier == name) {
-                return input
-            }
+    fun mapInputs(complexRefInputs: List<ComplexInput>, complexValInputs: List<ComplexInputAsValue>, literalInputs: List<LiteralInput>) : ProcessInput {
+        val referenceInputParams = HashMap<String, ReferenceParameter>()
+        val inlineInputParams = HashMap<String, InlineParameter>()
+
+        for(refInput in complexRefInputs){
+            referenceInputParams[refInput.wpsIdentifier] =
+                ReferenceParameter(refInput.wpsIdentifier, refInput.link, refInput.mimeType, refInput.encoding, refInput.xmlschema)
         }
-        return null;
+
+        for(litInput in literalInputs){
+            inlineInputParams[litInput.wpsIdentifier] = InlineParameter(litInput.wpsIdentifier, litInput.inputValue, defaultTextMimeType)
+        }
+
+        for(compValInput in complexValInputs){
+            inlineInputParams[compValInput.inputValue] = InlineParameter(compValInput.wpsIdentifier, compValInput.inputValue, compValInput.mimeType, compValInput.encoding, compValInput.xmlschema, InputType.COMPLEX)
+        }
+
+        return ProcessInput(wpsProcessIdentifier, inlineInputParams, referenceInputParams)
     }
 
-    protected fun getComplexValInputByName(name : String) : ComplexInputAsValue?{
-        for (input in complexValInputs) {
-            if (input.wpsIdentifier == name) {
-                return input
-            }
-        }
-        return null;
-    }
-
-    protected fun getLiteralInputByName(name : String) : LiteralInput?{
-        for (input in literalInputs) {
-            if (input.wpsIdentifier == name) {
-                return input
-            }
-        }
-        return null;
-    }
 }
--- asyncwrapper/src/main/kotlin/org/n/riesgos/asyncwrapper/process/wps/OutputMapper.kt
+++ asyncwrapper/src/main/kotlin/org/n/riesgos/asyncwrapper/process/wps/OutputMapper.kt
@@ -4,9 +4,21 @@ import org.n.riesgos.asyncwrapper.datamanagement.models.ComplexOutput
 import org.n.riesgos.asyncwrapper.process.ProcessInput
 import org.n.riesgos.asyncwrapper.process.ProcessOutput
 import java.net.PortUnreachableException
+import kotlin.random.Random
 
-abstract class OutputMapper(private val outputs : List<ProcessOutput>) {
+abstract class OutputMapper(private val jobId : Long,  private val output : ProcessOutput) {
 
-    abstract fun mapInputs() : List<ComplexOutput>
+    fun mapInputs() : List<ComplexOutput>{
+        val complexOutputs = ArrayList<ComplexOutput>()
+
+        for (refParams in output.referenceParameters){ //only complex reference outputs
+            for(refParam in refParams.value) {
+                val compOut = ComplexOutput(Random.nextLong(), jobId, refParams.key, refParam.link, refParam.mimeType, refParam.schema, refParam.encoding)
+                complexOutputs.add(compOut)
+            }
+        }
+
+        return complexOutputs;
+    }
 
 }
