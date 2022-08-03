07314dc1f3efcbe7a59e729d443e9ebf2bc3502f
Arne Vogt <a.vogt@52north.org>
2022-08-03 15:16:42 +0200

    add wrapper factory bean

---
+++ asyncwrapper/src/main/kotlin/org/n/riesgos/asyncwrapper/dummy/WrapperFactory.kt
@@ -0,0 +1,22 @@
+package org.n.riesgos.asyncwrapper.dummy
+
+import org.n.riesgos.asyncwrapper.config.AppConfiguration
+import org.n.riesgos.asyncwrapper.config.WPSConfiguration
+import org.n.riesgos.asyncwrapper.datamanagement.DatamanagementRepo
+import org.springframework.stereotype.Component
+import org.springframework.stereotype.Service
+
+@Service
+class WrapperFactory(val datamgmtRepo : DatamanagementRepo, val appConfig: AppConfiguration, val wpsConfig: WPSConfiguration) {
+
+    fun createWrapper() : AbstractWrapper{
+        val fullClassName = appConfig.wrapperClass
+        val classDef = Class.forName(fullClassName)
+        val cons = classDef.getConstructor(DatamanagementRepo::class.java)
+        val wrapperInst = cons.newInstance(datamgmtRepo) as AbstractWrapper
+
+        println("init wrapper " + wrapperInst.javaClass.name)
+
+        return wrapperInst;
+    }
+}
