2e89db92dbe69e901addf662324beb3d7816e3c3
Nils Brinckmann <nils.brinckmann@gfz-potsdam.de>
2023-04-26 11:22:54 +0200

    Migrated tests for the advanced deus logic to kotlin

--- asyncwrapper/src/test/kotlin/org/n/riesgos/asyncwrapper/dummy/utils/DeusUtilsTest.kt
+++ asyncwrapper/src/test/kotlin/org/n/riesgos/asyncwrapper/dummy/utils/DeusUtilsTest.kt
@@ -81,6 +81,182 @@ class DeusUtilsTest {
     }
 
     @Test
+    fun testCreatedWithLiteralInput() {
+        Assertions.assertNotNull(this.jdbcTemplate)
+        val template = this.jdbcTemplate!!
+
+        val gfzWpsUrl = "https://rz-vm140.gfz-potsdam.de/wps"
+        val modelpropProcessIdentifier = WPS_PROCESS_IDENTIFIER_MODELPROP
+        val assetmasterProcessIdentifier = WPS_PROCESS_IDENTIFIER_ASSETMASTER
+        val deusProcessIdentifier = "org.n52.gfz.riesgos.algorithm.impl.DeusProcess"
+
+
+        template.execute(
+                """
+                insert into processes(id, wps_url, wps_identifier)
+                values (1, '${gfzWpsUrl}', '${modelpropProcessIdentifier}')
+                """.trimIndent()
+        )
+        template.execute(
+                """
+                insert into processes(id, wps_url, wps_identifier)
+                values (2, '${gfzWpsUrl}', '${assetmasterProcessIdentifier}')
+                """.trimIndent()
+        )
+        template.execute(
+                """
+                insert into processes(id, wps_url, wps_identifier)
+                values (3, '${gfzWpsUrl}', '${deusProcessIdentifier}')
+                """.trimIndent()
+        )
+        // first call of modelprop
+        template.execute(
+                """
+                    insert into jobs (id, process_id, status)
+                    values (1, 1, 'success')
+                """.trimIndent()
+        )
+        val firstModelpropSchema = "HAZUS"
+        template.execute(
+                """
+                    insert into literal_inputs (id, wps_identifier, input_value, job_id)
+                    values (1, 'schema', '${firstModelpropSchema}', 1)
+                """.trimIndent()
+        )
+        template.execute(
+                """
+                    insert into literal_inputs (id, wps_identifier, input_value, job_id)
+                    values (2, 'assetcategory', 'buildings', 1)
+                """.trimIndent()
+        )
+        val complexOutputModelprop1 = ComplexOutput(1, 1, "selectedRows", "https://rz-vm140.gfz-potsdam.de/wps/results/modelprop/1", "application/json", "", "UTF-8")
+        template.execute(
+                """
+                    insert into complex_outputs (id, wps_identifier, mime_type, encoding, xmlschema, job_id, link)
+                    values (${complexOutputModelprop1.id}, '${complexOutputModelprop1.wpsIdentifier}',
+                           '${complexOutputModelprop1.mimeType}', '${complexOutputModelprop1.encoding}',
+                           '${complexOutputModelprop1.xmlschema}', ${complexOutputModelprop1.jobId},
+                           '${complexOutputModelprop1.link}')
+                """.trimIndent()
+        )
+
+        // second call of modelprop
+        template.execute(
+                """
+                    insert into jobs (id, process_id, status)
+                    values (2, 1, 'success')
+                """.trimIndent()
+        )
+        template.execute(
+                """
+                    insert into literal_inputs (id, wps_identifier, input_value, job_id)
+                    values (3, 'schema', 'SUPPASRI2023_v2.0', 2)
+                """.trimIndent()
+        )
+        template.execute(
+                """
+                    insert into literal_inputs (id, wps_identifier, input_value, job_id)
+                    values (4, 'assetcategory', 'buildings', 2)
+                """.trimIndent()
+        )
+        val complexOutputModelprop2 = ComplexOutput(2, 2, "selectedRows", "https://rz-vm140.gfz-potsdam.de/wps/results/modelprop/2", "application/json", "", "UTF-8")
+        template.execute(
+                """
+                    insert into complex_outputs (id, wps_identifier, mime_type, encoding, xmlschema, job_id, link)
+                    values (${complexOutputModelprop2.id}, '${complexOutputModelprop2.wpsIdentifier}',
+                           '${complexOutputModelprop2.mimeType}', '${complexOutputModelprop2.encoding}',
+                           '${complexOutputModelprop2.xmlschema}', ${complexOutputModelprop2.jobId},
+                           '${complexOutputModelprop2.link}')
+                """.trimIndent()
+        )
+        // The call of assetmaster
+        template.execute(
+                """
+                    insert into jobs (id, process_id, status)
+                    values (3, 2, 'success')
+                """.trimIndent()
+        )
+        val saraSchema = WPS_PROCESS_INPUT_IDENTIFIER_ASSETMASTER_SCHEMA_OPTIONS[0]
+        template.execute(
+                """
+                    insert into literal_inputs (id, wps_identifier, input_value, job_id)
+                    values (5, 'schema', '${saraSchema}', 3)
+                """.trimIndent()
+        )
+        template.execute(
+                """
+                    insert into literal_inputs (id, wps_identifier, input_value, job_id)
+                    values (6, 'querymode', 'intersects', 3)
+                """.trimIndent()
+        )
+        val complexOutputAssetmaster = ComplexOutput(3, 3, "selectedRowsGeoJson", "https://rz-vm140.gfz-potsdam.de/wps/results/assetmaster/3", "application/json", "", "UTF-8")
+        template.execute(
+                """
+                    insert into complex_outputs (id, wps_identifier, mime_type, encoding, xmlschema, job_id, link)
+                    values (${complexOutputAssetmaster.id}, '${complexOutputAssetmaster.wpsIdentifier}',
+                           '${complexOutputAssetmaster.mimeType}', '${complexOutputAssetmaster.encoding}',
+                           '${complexOutputAssetmaster.xmlschema}', ${complexOutputAssetmaster.jobId},
+                           '${complexOutputAssetmaster.link}')
+                """.trimIndent()
+        )
+        // And the deus earthquake job
+        template.execute(
+                """
+                    insert into jobs (id, process_id, status)
+                    values (4, 3, 'success')
+                """.trimIndent()
+        )
+        template.execute(
+                """
+                    insert into literal_inputs (id, wps_identifier, input_value, job_id)
+                    values (7, 'schema', 'SARA_v1.0', 4)
+                """.trimIndent()
+        )
+        template.execute(
+                """
+                    insert into complex_outputs_as_inputs (id, job_id, wps_identifier, complex_output_id)
+                    values (1, 4, 'exposure', 3)
+                """.trimIndent()
+        )
+        template.execute(
+                """
+                    insert into complex_outputs_as_inputs (id, job_id, wps_identifier, complex_output_id)
+                    values (2, 4, 'fragility', 1)
+                """.trimIndent()
+        )
+        val complexOutputForDeusEq = ComplexOutput(4, 4, "merged_output","https://rz-vm140.gfz-potsdam.de/wps/results/deus/4" ,"application/json", "", "UTF-8")
+        template.execute(
+                """
+                    insert into complex_outputs (id, wps_identifier, mime_type, encoding, xmlschema, job_id, link)
+                    values (${complexOutputForDeusEq.id}, '${complexOutputForDeusEq.wpsIdentifier}',
+                           '${complexOutputForDeusEq.mimeType}', '${complexOutputForDeusEq.encoding}',
+                           '${complexOutputForDeusEq.xmlschema}', ${complexOutputForDeusEq.jobId},
+                           '${complexOutputForDeusEq.link}')
+                """.trimIndent()
+        )
+
+
+        val datamanagementRepo = DatamanagementRepo(
+                template,
+                LiteralInputRepo(template),
+                ComplexInputRepo(template),
+                ComplexOutputAsInputRepo(template),
+                ComplexInputAsValueRepo(template),
+                BboxInputRepo(template),
+                OrderJobRefRepo(template),
+                ComplexOutputRepo(template),
+                OrderRepo(template),
+                StoredLinkRepo(template)
+        )
+
+        val deusUtils = DeusUtils(datamanagementRepo)
+
+        assertEquals(true, deusUtils.createdWithLiteralInput(complexOutputModelprop1, modelpropProcessIdentifier, "schema", listOf("HAZUS")))
+        assertEquals(false, deusUtils.createdWithLiteralInput(complexOutputModelprop1, modelpropProcessIdentifier, "schema", listOf("SUPPASRI2023_v2.0")))
+        assertEquals(true, deusUtils.createdWithLiteralInput(complexOutputModelprop2, modelpropProcessIdentifier, "schema", listOf("SUPPASRI2023_v2.0")))
+    }
+
+    @Test
     fun testIsDeusEqOutput () {
         Assertions.assertNotNull(this.jdbcTemplate)
         val template = this.jdbcTemplate!!
@@ -215,8 +391,8 @@ class DeusUtilsTest {
         template.execute(
                 """
                     insert into complex_outputs (id, wps_identifier, mime_type, encoding, xmlschema, job_id, link)
-                    values (${complexOutputForDeusEq.id}, '${complexOutputForDeusEq.wpsIdentifier}', 
-                           '${complexOutputForDeusEq.mimeType}', '${complexOutputForDeusEq.encoding}', 
+                    values (${complexOutputForDeusEq.id}, '${complexOutputForDeusEq.wpsIdentifier}',
+                           '${complexOutputForDeusEq.mimeType}', '${complexOutputForDeusEq.encoding}',
                            '${complexOutputForDeusEq.xmlschema}', ${complexOutputForDeusEq.jobId},
                            '${complexOutputForDeusEq.link}')
                 """.trimIndent()
@@ -245,8 +421,8 @@ class DeusUtilsTest {
         template.execute(
                 """
                     insert into complex_outputs (id, wps_identifier, mime_type, encoding, xmlschema, job_id, link)
-                    values (${complexOutputForDeusTs.id}, '${complexOutputForDeusTs.wpsIdentifier}', 
-                           '${complexOutputForDeusTs.mimeType}', '${complexOutputForDeusTs.encoding}', 
+                    values (${complexOutputForDeusTs.id}, '${complexOutputForDeusTs.wpsIdentifier}',
+                           '${complexOutputForDeusTs.mimeType}', '${complexOutputForDeusTs.encoding}',
                            '${complexOutputForDeusTs.xmlschema}', ${complexOutputForDeusTs.jobId},
                            '${complexOutputForDeusTs.link}')
                 """.trimIndent()
