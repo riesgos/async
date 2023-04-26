81b3dcd989531f9082eecb7f58a36be974c27fd4
Nils Brinckmann <nils.brinckmann@gfz-potsdam.de>
2023-04-26 08:54:37 +0200

    Cleanup

--- asyncwrapper/src/test/kotlin/org/n/riesgos/asyncwrapper/datamanagement/DatamanagementRepoTest.kt
+++ asyncwrapper/src/test/kotlin/org/n/riesgos/asyncwrapper/datamanagement/DatamanagementRepoTest.kt
@@ -1,22 +1,15 @@
 package org.n.riesgos.asyncwrapper.datamanagement
 
-import okhttp3.internal.filterList
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.n.riesgos.asyncwrapper.datamanagement.models.ComplexInputConstraint
-import org.n.riesgos.asyncwrapper.datamanagement.models.Process
 import org.n.riesgos.asyncwrapper.datamanagement.repos.*
-import org.n.riesgos.asyncwrapper.dummy.ModelpropEqWrapper
 import org.springframework.jdbc.core.JdbcTemplate
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType
 import org.springframework.test.annotation.DirtiesContext
 import java.io.File
-import java.io.FileInputStream
-import java.io.FileReader
-import java.io.InputStreamReader
-import java.nio.charset.Charset
 import java.nio.file.Files
 import java.nio.file.Paths
 import java.util.*
@@ -95,8 +88,6 @@ class DatamanagementRepoTest {
         val gfzWpsUrl = "https://rz-vm140.gfz-potsdam.de/wps"
         val modelpropProcessIdentifier = "org.n52.gfz.riesgos.algorithm.ModelpropProcess"
         val assetmasterProcessIdentifier = "org.n52.gfz.riesgos.algorithm.AssetmasterProcess"
-        val deusProcessIdentifier = "org.n52.gfz.riesgos.algorithm.DeusProcess"
-
 
         template.execute(
                 """
@@ -108,12 +99,6 @@ class DatamanagementRepoTest {
                 """
                 insert into processes(id, wps_url, wps_identifier)
                 values (2, '${gfzWpsUrl}', '${assetmasterProcessIdentifier}')
-                """.trimIndent()
-        )
-        template.execute(
-                """
-                insert into processes(id, wps_url, wps_identifier)
-                values (3, '${gfzWpsUrl}', '${deusProcessIdentifier}')
                 """.trimIndent()
         )
         // first call of modelprop
@@ -191,38 +176,6 @@ class DatamanagementRepoTest {
                 """
                     insert into complex_outputs (id, wps_identifier, mime_type, encoding, xmlschema, job_id, link)
                     values (3, 'selectedRowsGeoJson', 'application/json', 'UTF-8', '', 3, '${assetmasterOutputLink}')
-                """.trimIndent()
-        )
-        // And the deus earthquake job
-        template.execute(
-                """
-                    insert into jobs (id, process_id, status)
-                    values (4, 3, 'success')
-                """.trimIndent()
-        )
-        template.execute(
-                """
-                    insert into literal_inputs (id, wps_identifier, input_value, job_id)
-                    values (7, 'schema', 'SARA_v1.0', 4)
-                """.trimIndent()
-        )
-        template.execute(
-                """
-                    insert into complex_outputs_as_inputs (id, job_id, wps_identifier, complex_output_id)
-                    values (1, 4, 'exposure', 3)
-                """.trimIndent()
-        )
-        template.execute(
-                """
-                    insert into complex_outputs_as_inputs (id, job_id, wps_identifier, complex_output_id)
-                    values (2, 4, 'fragility', 1)
-                """.trimIndent()
-        )
-        val deusOutputLink = "https://rz-vm140.gfz-potsdam.de/wps/results/deus/4"
-        template.execute(
-                """
-                    insert into complex_outputs (id, wps_identifier, mime_type, encoding, xmlschema, job_id, link)
-                    values (4, 'merged_output', 'application/json', 'UTF-8', '', 4, '${deusOutputLink}')
                 """.trimIndent()
         )
 
